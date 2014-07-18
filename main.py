268c011ca35705416afb6a464607c8aa3674ce00
Podkopaev Anton <podkoav239@gmail.com>
2014-07-19 00:15:33 +0400

    Philosophy added

--- main.py
+++ main.py
@@ -27,26 +27,36 @@ with open(names_filename, 'rb') as cur_file:
 
 eng_file = 'http://abiturient.spbu.ru/data/asp_doc/for_lang.htm' 
 eng_filename = 'for_lang.htm'
-
 urllib.urlretrieve(eng_file, eng_filename)
 
-eng_scores = {}
-with open(eng_filename, 'rb') as cur_file:
-  soup = BeautifulSoup(cur_file.read())
-  table = soup.find('table')
-  rows = table.findAll('tr')
-  for tr in rows:
-    cols  = tr.findAll('td')
-    name  = cols[1].find(text = True)
-    score = cols[2].find(text = True)
-    if name == None:
-      continue
-    eng_scores[name] = score
+phil_file = 'http://abiturient.spbu.ru/data/asp_doc/filosofia.htm'
+phil_filename = 'filosofia.htm'
+urllib.urlretrieve(phil_file, phil_filename)
+
+i_scores = [{}, {}]
+file_number = 0
+for filename in [phil_filename, eng_filename]:
+  with open(filename, 'rb') as cur_file:
+    soup = BeautifulSoup(cur_file.read())
+    table = soup.find('table')
+    rows = table.findAll('tr')
+    for tr in rows:
+      cols  = tr.findAll('td')
+      name  = cols[1].find(text = True)
+      score = cols[2].find(text = True)
+      if name == None:
+        continue
+      name = name.replace('\n  ', ' ')
+      #print name
+      i_scores[file_number][name] = score
+  file_number += 1
 
 scores = {}
 for name in names:
-  eng_score = eng_scores.get(name, -1)
-  scores[name] = eng_score
+  try:
+      scores[name] = int(i_scores[0].get(name, -1)) + int(i_scores[1].get(name, -1))
+  except TypeError:
+      pass
 
 def tupComp(x, y):
   if x[1] < y[1]:
